b6959eff2d94363eb56535d5046be0b668f7c687
Meryem Melhaoui <melhaouimeryem1@gmail.com>
2025-07-02 00:53:15 +0100

    ajout des dernieres modifications pour le professeur

--- app.py
+++ app.py
@@ -11,25 +11,48 @@ def index():
     if request.method == "POST":
         try:
             modele_sel = request.form["modele"]
+titre_modele = modeles.get(modele_sel, modele_sel)
             champs = {
                 "nom":   request.form["nom"],
                 "date":  request.form["date"],
                 "objet": request.form["objet"],
                 "corps": request.form["corps"]
             }
+ # Récupérer le sexe sélectionné dans le formulaire
+            sexe = request.form.get("sexe")
+            if sexe == "femme":
+                civilite_signataire = "Madame"
+            elif sexe == "homme":
+                civilite_signataire = "Monsieur"
+            else:
+                civilite_signataire = ""
+
+            champs["civilite_signataire"] = civilite_signataire
+
+            if not all(champs.values()) or not civilite_signataire:
+                flash("Veuillez remplir tous les champs et sélectionner le sexe.", "warning")
+                return redirect(url_for("index"))
+
             if not all(champs.values()):
                 flash("Veuillez remplir tous les champs.", "warning")
                 return redirect(url_for("index"))
 
             nom_sortie = f"{champs['nom'].replace(' ', '_')}_{modele_sel}.docx"
             chemin = generer_lettre(f"{modele_sel}.docx", champs, nom_sortie)
-            return send_file(chemin, as_attachment=True)
-        except Exception as e:
+                return send_file(chemin, as_attachment=True)
+         except Exception as e:
             flash(f"Erreur : {e}", "danger")
-            return redirect(url_for("index"))
+                 return redirect(url_for("index"))
 
     # Pour l'affichage GET : liste des modèles disponibles
-    modeles = ["convocation", "reponse", "relance"]
+    modeles ={
+    "autorisation_construire": "Demande d'autorisation de construire",
+    "reponse_reclamation": "Réponse à une réclamation",
+    "relance_pieces": "Relance pour pièces manquantes",
+    "avis_defavorable": "Avis technique défavorable",
+    "convocation_commission": "Convocation à une commission d’instruction"
+}
+
     return render_template("index.html", modeles=modeles)
 
 if __name__ == "__main__":
